b045a1b76c4adb69141bb70e3ab3eafb69bcba09
mattferraz <mtshs.ferraz@gmail.com>
2022-07-28 23:18:34 -0300

    First commit

---
+++ src/main/java/com/tads/mhsf/bazaar/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.tads.mhsf.bazaar;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BazaarApplication.class);
+	}
+
+}
